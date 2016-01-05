35ea2219d3c6964e7116f3899dec4d85af87f885
mikhail.smirnou@gmail.com <mikhail.smirnou@gmail.com>
2016-01-05 16:07:34 +0300

    resultCommit

--- src/ThreadAdvancedExecutor.java
+++ src/ThreadAdvancedExecutor.java
@@ -9,10 +9,6 @@ public class ThreadAdvancedExecutor implements Executor {
     }
 
     public void dummyShit(){
-<<<<<<< HEAD
         System.out.println("I don't want these stupid things!");
-=======
-        System.out.println("dummyShitCalled with option!");
->>>>>>> 9cb680d795c5be60bd59ca24a31b45b29acb83dd
     }
 }
